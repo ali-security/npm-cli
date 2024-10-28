6c9bbf9a51743a5897d0240431fd78438605fb35
Alon Navon <alon.n@sealsecurity.io>
2024-10-28 23:34:08 +0200

    Parent doesn't matter to overrides

--- workspaces/arborist/lib/node.js
+++ workspaces/arborist/lib/node.js
@@ -837,9 +837,6 @@ class Node {
       target.root = root
     }
 
-    if (!this.overrides && this.parent && this.parent.overrides) {
-      this.overrides = this.parent.overrides.getNodeRule(this)
-    }
     // tree should always be valid upon root setter completion.
     treeCheck(this)
     if (this !== root) {
